86bca7b52934c58aa99443bbd6e5c1726a3e0c3a
Nicholas <nlamonaco86@gmail.com>
2020-10-16 17:29:55 -0400

    table rendered by JS

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,37 @@
-const getSize = () => {
-    document.getElementById("output").innerHTML = "Window size is: " + window.innerHeight + " X " + window.innerWidth + " pixels"
-    setInterval(getSize,500);
-} 
+const employees = [
+    {
+        id: 1,
+        first: "Mark",
+        last: "Otto",
+        position: "@mdo"
+    },
+    {
+        id: 2,
+        first: "Jacob",
+        last: "Thornton",
+        position: "@fat"
+    },
+    {
+        id: 3,
+        first: "Larry",
+        last: "the Bird",
+        position: "@twitter"
+    }]
 
-getSize()
+const renderTable = () => {
+    for (let i = 0; i < employees.length; i++) {
+        document.getElementById("employees").insertAdjacentHTML('beforebegin',
+            `<tr>
+            <td scope="col">${employees[i].id}</td>
+            <td scope="col">${employees[i].first}</td>
+            <td scope="col">${employees[i].last}</td>
+            <td scope="col">${employees[i].position}</td>
+            </tr>`)
+    }
+}
 
-console.warn("This is a warning message!")
+document.getElementById("firstname").addEventListener("click", () => {
+    console.log("click");
+});
+
+renderTable();
